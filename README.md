f2748b5bc1b557ccab4fafceef4e5fdb34844968
yuzaiakira <yuzai.akira@gmail.com>
2023-09-26 15:32:40 +0330

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Discord Chat Support Bot 
+this is a discord bot for chat support you can use this bot in your discord server
+this bot is developed by Python and the discord.py library. 
+
+## How To Install
+- first install library on `requirements.txt` file by command:
+```bash
+Akira@yuzai:~/Discord-Chat-Support-Bot$ pip install -r ./requirements.txt
+``` 
+- then rename `config-sample.json` file to `config.json` and modify value:
+```json 
+{
+    "bot_token": "your bot token",
+    "server_id": 1111111111111111111,
+    "text_channel_id": 1111111111111111111,
+    "reply_message": "your message is receive, and we are contact you very soon ",
+    "not_joined_server": "sorry you cant send message,\n please join to server then send message."
+}
+```
+- At the last run `main.py` file 
+
+I hope you enjoy this bot
---
+++ main.py
@@ -0,0 +1,58 @@
+import discord
+import json
+
+# load config.json 
+config_file = open('config.json', encoding="utf8")
+config = json.load(config_file)
+
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+client = discord.Client(intents=intents)
+
+
+server_id = config['server_id'] # your server id for check user
+text_channel = config['text_channel_id'] # text channel to send message
+
+
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+        
+        
+@client.event
+async def on_message(message):
+    # block bot message
+    if message.author.bot:
+        return
+    
+    # check message is on the bot DM
+    if isinstance(message.channel, discord.DMChannel):   
+        user_id = message.author.id
+        # guild server
+        server = client.get_guild(server_id) 
+        
+        # check members are in the server or not
+        if server.get_member(user_id) is not None:        
+            # member DM link
+            dm_link = f"https://discord.com/channels/@me/DMs/{user_id}"
+            
+            channel = client.get_channel(text_channel)
+            #  make embed for send user link
+            embed = discord.Embed()
+            embed.description = f" \n `author:` [{message.author}]({dm_link})"
+            
+            # send member message in text channel
+            await channel.send( f" `content:` {message.content} ", embed=embed)
+            # send waiting message to members 
+            await message.reply(config['reply_message'])
+            
+        else:
+            await message.reply(config['not_joined_server'])
+
+
+client.run(config['bot_token'])
